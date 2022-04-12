71093a564d518c4a55924816e511489a271ebfc2
William <williamwijaya.mail@gmail.com>
2022-04-12 21:33:46 +0000

    Animated Sentence

--- typewriter.js
+++ typewriter.js
@@ -1,24 +1,24 @@
 const sentence = "hello there from lighthouse labs";
 
-let delayTime = 50;
+// let delayTime = 50;
+// console.log replace with process.stdout.write
+// for (const char of sentence) {
+//   setTimeout(() => {
+//   process.stdout.write(char);
+// }, delayTime) // <= 1s delay to make it noticeable. Can dial it down later.
+// delayTime += 50;
+// }
+
+
+
+
+// //discussed
+let delayTime = 0;
 // console.log replace with process.stdout.write
 for (const char of sentence) {
   setTimeout(() => {
-  process.stdout.write(char);
-}, delayTime) // <= 1s delay to make it noticeable. Can dial it down later.
-delayTime += 50;
+    process.stdout.write(char);
+  }, delayTime += 10) // <= 1s delay to make it noticeable. Can dial it down later.
+  delayTime += 10;
 }
-
 setTimeout(() => console.log(), delayTime);
-
-
-
-// // need to be discussed
-// let delayTime = 0;
-// // console.log replace with process.stdout.write
-// for (const char of sentence) {
-//   setTimeout(() => {
-//     process.stdout.write(char);
-//   }, delayTime * 50) // <= 1s delay to make it noticeable. Can dial it down later.
-//   delayTime ++;
-// }
